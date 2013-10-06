3e262fac9c27b65e95ba4e34707406a8a50f4309
Ken Zheng <kzheng@ucdavis.edu>
2013-10-06 16:50:52 -0700

    added comments and created remember token before_create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
       if @user.save
         sign_in @user
         flash[:success] = "Welcome to the Sample App!"
+        # by default, Rails will try to render teh respective view, but create does not exist so we redirect
         redirect_to @user
       else
         render 'new'
@@ -20,7 +21,7 @@ class UsersController < ApplicationController
     end
     
     private
-      
+      # its not safe to mass assign params, so we use strong paramters
       def user_params
         params.require(:user).permit(:name, :email, :password, :password_confirmation)
       end
